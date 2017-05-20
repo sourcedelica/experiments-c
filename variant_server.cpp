89d2891f1da2c7c907329934210ca1eff85504a7
Eric Pederson <ericacm+github@gmail.com>
2017-05-20 17:13:54 -0400

    Make other components move-only.

--- variant_server.cpp
+++ variant_server.cpp
@@ -22,8 +22,8 @@ public:
     }
 
     ~CompositeServer() {
-        for (auto &tup : components) {
-            vrm::match(tup.second)([](auto &component){ component.close(); });
+        for (auto& tup : components) {
+            vrm::match(tup.second)([](auto& component){ component.close(); });
         }
     }
 
@@ -31,11 +31,10 @@ private:
     std::unordered_map<std::string, component_t> components;
 
     template <typename T>
-    void registerComponent(T &&policy) {
+    void registerComponent(T&& policy) {
         std::string name = policy.name();
         std::cout << "Registering: " << name << std::endl;
         components.emplace(name, std::move(policy));
-        auto i = 0;
     }
 
     template <typename T, typename ...Tz>
@@ -45,10 +44,10 @@ private:
     }
 
     void connectAll() {
-        for (auto &tup : components) {
+        for (auto& tup : components) {
             vrm::match(tup.second)(
-                [](auto &component) -> decltype(component.connect(), void()) { component.connect(); },
-                [](...) -> void {}
+                [](auto& component) -> decltype(component.connect(), void()) { component.connect(); },
+                [](auto& ...) -> void {}
             );
         }
     }
@@ -59,16 +58,16 @@ public:
     NetworkA() = default;                // Needed for variant
     NetworkA(const NetworkA&) = delete;  // Move-only
     NetworkA& operator=(NetworkA&) = delete;
-    // Note: when moving, make sure that when moved-from destructor runs it is safe
+    // Note: when moving, make sure that when moved-from destructor doesn't touch any moved-to data
     NetworkA(NetworkA&&) = default;
     NetworkA& operator=(NetworkA&& that) = default;
 
     template <typename ...Ts>
-    NetworkA(CompositeServer<Ts...> *server) : server(server) {}
+    NetworkA(CompositeServer<Ts...>* server) : server(server) {}
 
     ~NetworkA() { std::cout << "Destroying: " << Name << std::endl; }
 
-    static constexpr const char *Name = "NetworkA";
+    static constexpr const char* Name = "NetworkA";
 
     std::string name() { return Name; }
 
@@ -84,12 +83,18 @@ private:
 
 class NetworkB {
 public:
-    NetworkB() = default;
-
+    NetworkB() = default;                
+    NetworkB(const NetworkB&) = delete;  
+    NetworkB& operator=(NetworkB&) = delete;
+    NetworkB(NetworkB&&) = default;
+    NetworkB& operator=(NetworkB&&) = default;
+    
     template <typename ...Ts>
-    NetworkB(CompositeServer<Ts...> *server) : server(server) {}
+    NetworkB(CompositeServer<Ts...>* server) : server(server) {}
 
-    static constexpr const char *Name = "NetworkB";
+    ~NetworkB() { std::cout << "Destroying: " << Name << std::endl; }
+
+    static constexpr const char* Name = "NetworkB";
 
     std::string name() { return Name; }
 
@@ -106,11 +111,17 @@ private:
 class Database {
 public:
     Database() = default;
+    Database(const Database&) = delete;
+    Database& operator=(Database&) = delete;
+    Database(Database&&) = default;
+    Database& operator=(Database&&) = default;
 
     template <typename ...Ts>
-    Database(CompositeServer<Ts...> *server) : server(server) {}
+    Database(CompositeServer<Ts...>* server) : server(server) {}
 
-    static constexpr const char *Name = "Database";
+    ~Database() { std::cout << "Destroying: " << Name << std::endl; }
+
+    static constexpr const char* Name = "Database";
 
     std::string name() { return Name; }
 
